8d79a1f196db1ed45b5252615787bcc4057b29a8
musabash <basmusa@icloud.com>
2021-04-03 23:57:31 +0100

    edit-cancel buttons updated

--- src/components/blog-details.js
+++ src/components/blog-details.js
@@ -14,42 +14,43 @@ const BlogDetails = () => {
   const [title, setTitle] = useState(blog.title)
   const isAuthorised = user.displayName === blog.author
 
-  function BlogBody() {
-    return (
-      <div className="blog-body">{body.map((elm, index) =>
-        isEditable ?
-          <BlogParagraph
-            key={index}
-            index={index}
-            par={elm}
-            body={body}
-            setBody={setBody}
-          /> :
-        <p key={index}>{elm}</p> 
-      )}
-      </div>
-    )
+  function EditButton({name}) {
+    return <button onClick={() => setIsEditable(prev => !prev)}>{name}</button>
+  }
+
+  const handleUpdate = () => {
+    updateBlog(title, body, id)
+    setIsEditable(prev => !prev)
   }
 
   return (
     <div className="blog-details">
-        <article>
-          {isEditable ? <Title title={title} setTitle={setTitle} /> :
-          <h2>{title}</h2>}
-          <p className="blog-author">Written by {blog.author}</p>
-          <p className="blog-date">{blog.date}</p>
-          <BlogBody />
-          {isAuthorised && <DeleteButton id={id} deleteBlog={deleteBlog}/>}
-          
-          {isEditable && isAuthorised &&
-            <button onClick={(e) => {
-              updateBlog(title, body, id)
-              setIsEditable(prev => !prev)
-            }}>save</button>}
-            {!isEditable && isAuthorised &&
-            <button onClick={() => setIsEditable(prev => !prev)}>edit</button>
-          }
-        </article>
+      <article>
+        {isEditable ? <Title title={title} setTitle={setTitle} /> :
+        <h2>{title}</h2>}
+        <p className="blog-author">Written by {blog.author}</p>
+        <p className="blog-date">{blog.date}</p>
+        <div className="blog-body">{body.map((elm, index) =>
+          isEditable ?
+            <BlogParagraph
+              key={index}
+              index={index}
+              par={elm}
+              body={body}
+              setBody={setBody}
+            /> :
+          <p key={index}>{elm}</p> 
+          )}
+        </div>
+        
+        {isAuthorised && <EditButton name={isEditable ? "cancel" : "edit"} />}
+        {isAuthorised && isEditable && 
+          <>
+            <DeleteButton id={id} deleteBlog={deleteBlog}/>
+            <button onClick={handleUpdate}>save</button>
+          </>
+        }            
+      </article>
     </div>
    );
 }
